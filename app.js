6e68b5a931b85f3cca105508e7c1d6e629af2515
Prankur Pandey <pprankur@gmail.com>
2021-09-08 05:53:10 +0000

    updated logic ad fixed bugs

--- app.js
+++ app.js
@@ -5,39 +5,35 @@ const noOfNotes = document.querySelectorAll('.noofnotes')
 const outputText = document.querySelector('#output-text')
 var notes = [2000, 500, 100, 20, 10, 5, 1]
 
-function checkValidAmount()
+function checkValidAmount() 
 {
-
- if (cashAmount.value > 0)
- {
-   if(cashAmount.value > billAmount.value)
-   {
-    checkAmount()
-   }
-   else
-   {
-     displayMessage('please wash plates')
-   }
- }
-}
-
-function checkAmount() {
-  const remainingAmount = Number(cashAmount.value) - Number(billAmount.value)
-  outputText.innerHTML = remainingAmount;
-  calculateNotes(remainingAmount) 
-  return remainingAmount;
+	
+		const remainingAmount = Number(cashAmount.value) - Number(billAmount.value)
+		outputText.innerHTML = 'amount to be returned :' + remainingAmount;
+		calculateNotes(remainingAmount)
+	if ( billAmount.value > cashAmount.value)
+	{
+		
+		const remainingAmount = Number(cashAmount.value) - Number(billAmount.value)
+		displayMessage('amount to be returned :' + remainingAmount)
+		calculateNotes(remainingAmount)
+	}
+	else {
+		displayMessage('invalid')
+	}
+		
 }
 
 function calculateNotes(remainingAmount) {
-  for (index = 0; index < notes.length; index++) {
-    console.log(notes[index])
-    const amountToBeReturned = Math.trunc(Number(remainingAmount) / Number(notes[index]))
-    console.log(amountToBeReturned)
-    remainingAmount = remainingAmount % notes[index]
-    noOfNotes[index].innerHTML = amountToBeReturned
-  }
+	for (index = 0; index < notes.length; index++) {
+		console.log(notes[index])
+		const amountToBeReturned = Math.trunc(Number(remainingAmount) / Number(notes[index]))
+		console.log(amountToBeReturned)
+		remainingAmount = remainingAmount % notes[index]
+		noOfNotes[index].innerHTML = amountToBeReturned
+	}
 }
- function displayMessage(message){
-   outputText.innerHTML = message;
- }
+function displayMessage(message) {
+	outputText.innerHTML = message;
+}
 button.addEventListener('click', checkValidAmount)
